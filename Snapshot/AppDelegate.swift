bbc8a950e6dc15fb80afcf858919ea31932c2c8e
caoyunhao <1067233@gmail.com>
2019-11-15 19:16:08 +0800

    Initial Commit

---
+++ Snapshot/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Snapshot
+//
+//  Created by Yunhao on 2019/11/15.
+//  Copyright © 2019 Yunhao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
